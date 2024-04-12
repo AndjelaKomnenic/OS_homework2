0bacd4c35425db95bcc5df6100d8a38935bcba4b
AndjelaKomnenic <komnenic.andjela@gmail.com>
2024-04-12 23:10:20 +0200

    dodata proera kada nije symlink

--- user/symlinkinfo.c
+++ user/symlinkinfo.c
@@ -7,55 +7,51 @@
 char*
 fmtname(char *path)
 {
-	static char buf[DIRSIZ+1];
-	char *p;
+    static char buf[DIRSIZ+1];
+    char *p;
 
-	// Find first character after last slash.
-	for(p=path+strlen(path); p >= path && *p != '/'; p--)
-		;
-	p++;
+    // Find first character after last slash.
+    for(p=path+strlen(path); p >= path && *p != '/'; p--)
+        ;
+    p++;
 
-	// Return blank-padded name.
-	if(strlen(p) >= DIRSIZ)
-		return p;
-	memmove(buf, p, strlen(p));
-	memset(buf+strlen(p), ' ', DIRSIZ-strlen(p));
-	return buf;
+    // Return blank-padded name.
+    if(strlen(p) >= DIRSIZ)
+        return p;
+    memmove(buf, p, strlen(p));
+    memset(buf+strlen(p), ' ', DIRSIZ-strlen(p));
+    return buf;
 }
 
 void
 symlinkinfo(char *path)
 {
-	char buf[512], *p;
-	int fd;
-	struct dirent de;
-	struct stat st;
+    char buf[512], *p;
+    int fd;
+    struct dirent de;
+    struct stat st;
 
-	if((fd = open(path, O_NOFOLLOW)) < 0){
-		fprintf(2, "ls: cannot open %s\n", path);
-		return;
-	}
+    if((fd = open(path, O_NOFOLLOW)) < 0){
+        fprintf(2, "ls: cannot open %s\n", path);
+        return;
+    }
 
-	if(fstat(fd, &st) < 0){
-		fprintf(2, "ls: cannot stat %s\n", path);
-		close(fd);
-		return;
-	}
+    if(fstat(fd, &st) < 0){
+        fprintf(2, "ls: cannot stat %s\n", path);
+        close(fd);
+        return;
+    }
 
-	switch(st.type){
-        case T_SYMLINK:
-            char buf2[512];
-            read(fd,buf2,sizeof(buf2));
-            printf("%s        -> %s\n", fmtname(path), buf2);
-            break;
-
-        case T_DIR:
-            if(strlen(path) + 1 + DIRSIZ + 1 > sizeof buf){
-                printf("ls: path too long\n");
-                break;
-            }
+    if(st.type == T_SYMLINK) {
+        char buf2[512];
+        read(fd, buf2, sizeof(buf2));
+        printf("%s -> %s\n", fmtname(path), buf2);
+    } else if(st.type == T_DIR) {
+        if(strlen(path) + 1 + DIRSIZ + 1 > sizeof buf){
+            printf("ls: path too long\n");
+        } else {
             strcpy(buf, path);
-            p = buf+strlen(buf);
+            p = buf + strlen(buf);
             *p++ = '/';
             while(read(fd, &de, sizeof(de)) == sizeof(de)){
                 if(de.inum == 0)
@@ -66,31 +62,32 @@ symlinkinfo(char *path)
                     printf("ls: cannot stat %s\n", buf);
                     continue;
                 }
-                if(st.type==T_SYMLINK)
-                {
+                if(st.type == T_SYMLINK) {
                     int fd1;
-                    fd1 = open(buf, O_NOFOLLOW);
                     char buf1[512];
-                    read(fd1,buf1,sizeof(buf1));
-                    printf("%s        -> %s\n", fmtname(buf), buf1);
+                    fd1 = open(buf, O_NOFOLLOW);
+                    read(fd1, buf1, sizeof(buf1));
+                    printf("%s -> %s\n", fmtname(buf), buf1);
                     close(fd1);
                 }
             }
-            break;
-	}
-	close(fd);
+        }
+    } else {
+        printf("%s -> not a symbolic link\n", fmtname(path));
+    }
+    close(fd);
 }
 
 int
 main(int argc, char *argv[])
 {
-	int i;
+    int i;
 
-	if(argc < 2){
-		symlinkinfo(".");
-		exit();
-	}
-	for(i=1; i<argc; i++)
-		symlinkinfo(argv[i]);
-	exit();
+    if(argc < 2){
+        symlinkinfo(".");
+        exit();
+    }
+    for(i=1; i<argc; i++)
+        symlinkinfo(argv[i]);
+    exit();
 }
